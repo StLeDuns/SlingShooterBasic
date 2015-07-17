b6981039ff7a691adab117895ffc19ccd634b224
StLeDuns <lena_chan@hotmail.de>
2015-07-17 11:54:57 +0200

    Test

--- Assets/Scripts/followCam.cs
+++ Assets/Scripts/followCam.cs
@@ -45,12 +45,6 @@ public class FollowCam : MonoBehaviour {
 			destination.y = 35.0f;
 				}
 
-		
-		if(destination.x >= 360.0f){
-			destination.x = 360.0f;
-		}
-
-
 		// Limit the X and Y to minimum values
 		destination.x = Mathf.Max (minXY.x, destination.x);
 		destination.y = Mathf.Max(minXY.y, destination.y);
@@ -67,5 +61,4 @@ public class FollowCam : MonoBehaviour {
 		// Set OrthographicSize of camera to keep the ground in view
 		this.GetComponent<Camera>().orthographicSize = destination.y + 10;
 	}
-
 }
